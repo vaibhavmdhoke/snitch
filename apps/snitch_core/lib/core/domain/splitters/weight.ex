1768e090feebce2dc9f4e8016dadd64acebab2f9
Sagar Karwande <sagar_karwande123@yahoo.com>
2018-05-15 15:32:49 +0530

    splitting logic

--- apps/snitch_core/lib/core/domain/splitters/weight.ex
+++ apps/snitch_core/lib/core/domain/splitters/weight.ex
@@ -1,5 +1,5 @@
 defmodule Snitch.Core.Domain.Splitters.Weight do
-  @default_threshold Decimal.new(150)
+  @bin_threshold 7
 
   def split(packages) do
     packages
@@ -12,6 +12,34 @@ defmodule Snitch.Core.Domain.Splitters.Weight do
       |> Enum.sort(&compare_weight/2)
 
     %{package | items: sorted_line_items}
+  end
+
+  def add_item_to_bin(item, []) do
+    [[item]]
+  end
+
+  def add_item_to_bin(item, acc) do
+    put_in_bin(item, acc, acc)
+  end
+
+  def put_in_bin(item, [], acc) do
+    [[item] | acc]
+  end
+
+  def put_in_bin(item, [first_bin | rest_bins], acc) do
+    total = first_bin |> Enum.reduce(0, fn item, acc -> item + acc end)
+
+    next_total = total + item
+
+    case next_total <= @bin_threshold do
+      true ->
+        update_bin = [item | first_bin]
+        temp = List.delete(acc, first_bin)
+        [update_bin | temp]
+
+      false ->
+        put_in_bin(item, rest_bins, acc)
+    end
   end
 
   def compare_weight(item1, item2) do
